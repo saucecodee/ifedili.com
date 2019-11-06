288c0ae5e399877017dbbbae753714c5e0c922b0
saucecodee <ashawinta@gmail.com>
2019-11-07 00:22:40 +0100

    Did some stuff, lol

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 document.querySelector("#menu-btn").addEventListener("click", ()=> {
      document.querySelector("header").classList.toggle("mobile-header");
+     document.querySelector("main").classList.toggle("blur");
 })
