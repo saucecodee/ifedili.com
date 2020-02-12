a741b4fe7bd881df70e3ac498d6c3de9147f51b1
saucecodee <ashawinta@gmail.com>
2020-02-12 13:45:45 +0100

    Fixed stagnant spalsh screen on big screen

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-document.querySelector("#menu-btn").addEventListener("click", ()=> {
+window.onload = () => {
+     console.log("loaded")
+     // document.querySelector("#intro").style.display = "none";
+}
+
+document.querySelector("#menu-btn").addEventListener("click", () => {
      document.querySelector("header").classList.toggle("mobile-header");
      document.querySelector("main").classList.toggle("blur");
 })
