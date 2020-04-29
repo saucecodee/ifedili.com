2cb950134f82cb11890f8bffa78b6dcc5ae6da5a
saucecodee <ashawinta@gmail.com>
2020-04-29 03:29:00 +0100

    cleared old code

--- js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-     console.log("loaded")
-     // document.querySelector("#intro").style.display = "none";
-}
-
-document.querySelector("#menu-btn").addEventListener("click", () => {
-     document.querySelector("header").classList.toggle("mobile-header");
-     document.querySelector("main").classList.toggle("blur");
-})
