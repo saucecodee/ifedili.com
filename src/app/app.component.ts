04434e1fc334b89efb4f430c7bfcf4e876531569
saucecodee <ashawinta@gmail.com>
2021-10-22 11:44:13 +0100

    style: add initial component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ifedili';
+
+  $ = (s: any) => document.querySelector(s)
+
+  exitMenu() {
+    this.$("#menu").classList.add("exit")
+    setTimeout(() => {
+      this.$("#menu").classList.remove("exit")
+      this.$("#menu").style.transform = "scale(0)";
+      this.$("#menu").style.opacity = "0";
+    }, 500);
+    console.log("exit")
+  }
+
+  showMenu() {
+    this.$("#menu").style.transform = "scale(1)";
+    this.$("#menu").style.opacity = "1";
+    console.log("open")
+  }
 }
