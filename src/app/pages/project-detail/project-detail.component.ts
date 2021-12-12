6abf668f3466fea4f9d8357581246cde0fd63b1d
saucecodee <ashawinta@gmail.com>
2021-12-12 11:19:03 +0100

    integrate project detais component with data store

--- src/app/pages/project-detail/project-detail.component.ts
+++ src/app/pages/project-detail/project-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { projects } from 'src/app/core/data';
+import { IProject } from 'src/app/core/models';
 
 @Component({
   selector: 'app-project-detail',
@@ -7,10 +9,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./project-detail.component.scss']
 })
 export class ProjectDetailComponent implements OnInit {
-  project_id: string;
+  id: string;
+  project: IProject
 
   constructor(private actRoute: ActivatedRoute) {
-    this.project_id = this.actRoute.snapshot.params.project_id;
+    this.id = this.actRoute.snapshot.params.project_id;
+    this.project =  projects[this.id]
   }
 
   ngOnInit(): void {
