af9e1478d4825846a5dd9e3d722b19541bab901d
saucecodee <ashawinta@gmail.com>
2021-12-29 14:57:33 +0100

    chore: populate experience list

--- src/app/core/data/experiences.ts
+++ src/app/core/data/experiences.ts
@@ -5,28 +5,49 @@ export default [
     from: "Aug 2020",
     to: "Present",
     company: "Genesys Tech Hub",
-    description: "string",
+    description: "Designing and building server-side application services and APIs",
     role: "Software Engineer"
   },
   {
     from: "May 2021",
     to: "Oct 2021",
     company: "Huobi Global",
-    description: "string",
+    description: "Built web oracle that stands between the client and smart contract for an NFT marketplace.",
     role: "Backend Engineer"
   },
   {
     from: "May 2019",
     to: "Jun 2020",
     company: "HNG Internship",
-    description: "string",
-    role: "Backend Engineer"
+    description: "I built and deployed dockerized server-side applications and  microservices on AWS.",
+    role: "Intern Backend Engineer/DevOps"
   },
   {
-    from: "Nov 2018",
+    from: "Jul 2019",
+    to: "Dec 2019",
+    company: "Genesys Tech Hub",
+    description: "I worked on server-side applications with NodeJS and MongoDB to deploy applications.",
+    role: "Intern Backend Engineer"
+  },
+  {
+    from: "May 2019",
+    to: "Jul 2019",
+    company: "HNG Internship",
+    description: "Collaborated remotely on projects using PHP on the backend and HTML, CSS, Javascript and Bootstrap on the frontend.",
+    role: "Fullstack Developer(Intern)"
+  },
+  {
+    from: "Oct 2015",
+    to: "Jul 2020",
+    company: "Enugu State University of Science and Technology",
+    description: "Studied Computer engineering and got my B.Eng",
+    role: "Student"
+  },
+  {
+    from: "2017",
     to: "Present",
     company: "Freelance",
-    description: "string",
-    role: "Fullstack Engineer"
-  }
+    description: "I help bring various clients’ visions into reality through seamless process and on time delivery.",
+    role: "Fullstack Developer"
+  },
 ] as IExperience[]
