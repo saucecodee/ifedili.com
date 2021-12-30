f23381c472c831eea0298bcb035615688f9dcf9d
saucecodee <ashawinta@gmail.com>
2021-12-30 22:23:47 +0100

    feat: populate skills list and images

--- src/app/core/data/tools.ts
+++ src/app/core/data/tools.ts
@@ -1,46 +1,160 @@
 import { ITool } from "../models"
 
 export default {
+  banga: {
+    id: "banga",
+    image: "./../../../../assets/skills/bangajs.png",
+    name: "BàngáJS",
+    categories: ["Web"]
+  },
   ang: {
-    id: "ng",
-    image: "./../../../../assets/skills/nodejs.png",
+    id: "ang",
+    image: "./../../../../assets/skills/angular.png",
     name: "Angular",
     categories: ["Web"]
   },
   nod: {
-    id: "nd",
+    id: "nod",
     image: "./../../../../assets/skills/nodejs.png",
     name: "NodeJS",
-    categories: ["Web", "Languages"]
+    categories: ["Web"]
   },
-  ion: {
-    id: "ng",
-    image: "./../../../../assets/skills/nodejs.png",
+  ex: {
+    id: "ex",
+    image: "./../../../../assets/skills/express.png",
+    name: "ExpressJS",
+    categories: ["Web"]
+  },
+  ionic: {
+    id: "ionic",
+    image: "./../../../../assets/skills/ionic.png",
     name: "Ionic",
-    categories: ["Web"]
+    categories: ["Others"]
   },
-  tsc: {
-    id: "ng",
-    image: "./../../../../assets/skills/nodejs.png",
+  ts: {
+    id: "ts",
+    image: "./../../../../assets/skills/typescript.svg",
     name: "TypeScript",
-    categories: ["Web"]
+    categories: ["Languages"]
   },
   js: {
-    id: "ng",
-    image: "./../../../../assets/skills/nodejs.png",
+    id: "js",
+    image: "./../../../../assets/skills/javascript.png",
     name: "JavaScript",
-    categories: ["Web"]
+    categories: ["Languages"]
   },
   sol: {
-    id: "ng",
-    image: "./../../../../assets/skills/nodejs.png",
+    id: "sol",
+    image: "./../../../../assets/skills/solidity.png",
     name: "Solidity",
-    categories: ["Web"]
+    categories: ["Languages", "Blockchain"]
   },
   go: {
-    id: "ng",
-    image: "./../../../../assets/skills/nodejs.png",
+    id: "go",
+    image: "./../../../../assets/skills/golang.png",
     name: "Go",
-    categories: ["Web", "Blockchain"]
+    categories: ["Languages"]
+  },
+  mongo: {
+    id: "mongo",
+    image: "./../../../../assets/skills/mongodb.png",
+    name: "MongoDB",
+    categories: ["Web"]
+  },
+  postg: {
+    id: "postg",
+    image: "./../../../../assets/skills/postgre.png",
+    name: "PostgreSQL",
+    categories: ["Web"]
+  },
+  redis: {
+    id: "redis",
+    image: "./../../../../assets/skills/redis.png",
+    name: "Redis",
+    categories: ["Web"]
+  },
+  fbase: {
+    id: "fbase",
+    image: "./../../../../assets/skills/firebase.png",
+    name: "Firebase",
+    categories: ["Web"]
+  },
+  aws: {
+    id: "aws",
+    image: "./../../../../assets/skills/aws.png",
+    name: "AWS",
+    categories: ["Web"]
+  },
+  html: {
+    id: "html",
+    image: "./../../../../assets/skills/html.png",
+    name: "HTML",
+    categories: ["Languages"]
+  },
+  css: {
+    id: "css",
+    image: "./../../../../assets/skills/css.png",
+    name: "CSS",
+    categories: ["Languages"]
+  },
+  scss: {
+    id: "scss",
+    image: "./../../../../assets/skills/scss.png",
+    name: "SCSS",
+    categories: ["Web"]
+  },
+  gsap: {
+    id: "gsap",
+    image: "./../../../../assets/skills/gsap.svg",
+    name: "GSAP",
+    categories: ["Web"]
+  },
+  dock: {
+    id: "dock",
+    image: "./../../../../assets/skills/docker.png",
+    name: "Docker",
+    categories: ["Others"]
+  },
+  nest: {
+    id: "nest",
+    image: "./../../../../assets/skills/nestjs.png",
+    name: "NestJS",
+    categories: ["Web"]
+  },
+  sql: {
+    id: "sql",
+    image: "./../../../../assets/skills/sql.png",
+    name: "SQL",
+    categories: ["Languages"]
+  },
+  react: {
+    id: "react",
+    image: "./../../../../assets/skills/react.png",
+    name: "ReactJS",
+    categories: ["Web"]
+  },
+  ejs: {
+    id: "ejs",
+    image: "./../../../../assets/skills/ejs.jpeg",
+    name: "EJS",
+    categories: ["Web"]
+  },
+  php: {
+    id: "php",
+    image: "./../../../../assets/skills/php.png",
+    name: "PHP",
+    categories: ["Languages"]
+  },
+  psd: {
+    id: "psd",
+    image: "./../../../../assets/skills/photoshop.png",
+    name: "Photoshop",
+    categories: ["Design"]
+  },
+  figma: {
+    id: "figma",
+    image: "./../../../../assets/skills/figma.png",
+    name: "Figma",
+    categories: ["Design"]
   },
 } as { [id: string]: ITool }
